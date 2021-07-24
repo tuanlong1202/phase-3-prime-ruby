277810dff5a7f0519b773dec3f7f9a43be2772cb
tuanlong1202 <tuanlong1202@gmail.com>
2021-07-24 15:15:56 -0400

    Phase 3 prime ruby

--- prime.rb
+++ prime.rb
@@ -1,1 +1,38 @@
 # Add  code here!
+
+def initArray(size)
+    retArray = []
+    i = 2
+    while i <= size
+        retArray << i
+        i += 1
+    end
+    retArray
+end
+
+def divisible(number)
+    i = 2
+    retBln = false
+    while i < ((number/2)+1)
+        if number%i == 0
+            retBln = true
+            break
+        end
+        i += 1
+    end
+    retBln
+end
+
+def prime?(number)
+    if number < 2
+        return false
+    else
+        numberArray = initArray(number)
+        numberArray.each_with_index do |num,index|
+            if divisible(num)
+                numberArray[index] = 0
+            end
+        end
+        return numberArray.include?(number)
+    end
+end
